61b92e0f7acee9e3a9f1aacccdbfe8d56ce2221f
AyelDev <aarielabelgas23@gmail.com>
2023-12-08 10:48:16 +0800

    first update for DB web project

---
+++ SeniorSystemDBS/login.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- color pallete -->
+    <!-- https://coolors.co/palette/ff9f1c-ffbf69-ffffff-cbf3f0-2ec4b6 -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign-in</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
+    <link rel="stylesheet" href="/SeniorSystemDBS/css/style.css">
+</head>
+
+<body>
+
+    <div class="container-sm">
+        <div class="row">
+            <div class="col h-100" style="margin-top:6%">
+                <!-- Login Card -->
+                <div class="card rounded-5 text-bg-light" style="padding:4%">
+                    <!-- image and form -->
+                    <div class="container px-4">
+                        <div class="row gx-5 align-items-center">
+                            <div class="col">
+                                <div class="p-3">
+                                    <img class="imageFront" src="/SeniorSystemDBS/image/seniorFrontLogin.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="p-3">
+                                    <h1 class="text-center">Sign-in</h1>
+                                </div>
+                                <!-- ////// forms for text //////// -->
+                                <form>
+                                    <div class="mb-3">
+                                        <label class="form-label">Username</label>
+                                        <input type="email" class="form-control border border-warning" id="inputUsername">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                                        <input type="password" class="form-control border border-warning" id="exampleInputPassword1">
+                                    </div>
+                                    <div class="mb-3 form-check">
+                                        <input type="checkbox" class="form-check-input border border-warning" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Show Password</label>
+                                    </div>
+                                    <button type="submit" class="btn" style="background-color: #2EC4B6">Sign-in</button>
+                                </form>
+                                <!-- //// -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- //// -->
+
+                    <p class="card-text"><small>&copy; DB31 Project By: Ariel Abelgas, Cindy Mae Labra, Allyza Indiola</small></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ SeniorSystemDBS/userIndex.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign-up</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container-sm">
+  <!-- Content here -->
+  <nav class="navbar navbar-expand-lg rounded-4" data-bs-theme="light" style="background-color:#2EC4B6;">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white m-1" href="#">Name</a>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6">Master List
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+      
+          <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#" style="border-color:#2EC4B6;">Payout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6;">Contact Support</a>
+        </li>   
+      </ul>
+      <form class="d-flex">
+        <a href="login.php" class="btn btn-outline-warning text-white my-2 my-sm-0" style="border-color: #2EC4B6">Sign-out</a>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
