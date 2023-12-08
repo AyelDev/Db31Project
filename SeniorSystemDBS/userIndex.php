62bc891118d537b5ae50884ef00ced77be64b447
CINDYLABSS\peterjohn <cindymae2018@gmail.com>
2023-12-08 14:23:29 +0800

    update for db project

--- SeniorSystemDBS/userIndex.php
+++ SeniorSystemDBS/userIndex.php
@@ -2,46 +2,71 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sign-up</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sign-up</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css"
+    integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
+  <link rel="stylesheet" href="/DB31Project/SeniorSystemDBS/css/userindexstyle.css">
 </head>
+
 <body>
 
-<div class="container-sm">
-  <!-- Content here -->
-  <nav class="navbar navbar-expand-lg rounded-4" data-bs-theme="light" style="background-color:#2EC4B6;">
-  <div class="container-fluid">
-    <a class="navbar-brand text-white m-1" href="#">Name</a>
-    <div class="collapse navbar-collapse" id="navbarColor03">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6">Master List
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-      
-          <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#" style="border-color:#2EC4B6;">Payout</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6;">Contact Support</a>
-        </li>   
-      </ul>
-      <form class="d-flex">
-        <a href="login.php" class="btn btn-outline-warning text-white my-2 my-sm-0" style="border-color: #2EC4B6">Sign-out</a>
-      </form>
-    </div>
-  </div>
-</nav>
-</div>
+  <div class="container-xl">
+    <!-- Content here -->
+    <nav class="navbar fixed-top navbar-expand-lg rounded-4" data-bs-theme="light" style="background-color:#2EC4B6;">
+      <div class="container-fluid">
+        
+        <a class="navbar-brand text-white m-1" href="#">Name</a>
+        <div class="collapse navbar-collapse" id="navbarColor03">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6">Master
+                List
+                <span class="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
+              <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#"
+                style="border-color:#2EC4B6;">Payout</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6;">Contact
+                Support</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <a href="login.php" class="btn btn-outline-warning text-white my-2 my-sm-0"
+              style="border-color: #2EC4B6">Sign-out</a>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div class="main">
+      <img class="main_image" src="/DB31Project/SeniorSystemDBS/image/main_image.png" alt="">
+      <div class="right_box0">
+
+      </div>
+      <div class="right_box1">
+        <h1>.</h1>
+      </div>
+      <div class="right_box2">
+        
+      </div>
+      <div class="text">Elderly Friendly Management System</div>
+      <div class="text2">Supporting seniors with care and practical help to improve their well-being and independence.</div>
+      <div class="logo"><img src="/DB31Project/SeniorSystemDBS/image/logo2.png" alt=""></div>
+      <button class="login_btn" role="button" type="button">Login</button>
+    </div>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
