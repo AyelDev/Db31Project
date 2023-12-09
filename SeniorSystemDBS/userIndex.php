79286026f953276cd8d1cf981e4affee48bb1c5c
CINDYLABSS\peterjohn <cindymae2018@gmail.com>
2023-12-09 17:10:17 +0800

    another updates for db system

--- SeniorSystemDBS/userIndex.php
+++ SeniorSystemDBS/userIndex.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sign-up</title>
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css"
@@ -19,22 +21,24 @@
     <nav class="navbar fixed-top navbar-expand-lg rounded-4" data-bs-theme="light" style="background-color:#2EC4B6;">
       <div class="container-fluid">
         
-        <a class="navbar-brand text-white m-1" href="#">Name</a>
+        <a class="navbar-brand text-white m-1" href="#">Home</a>
         <div class="collapse navbar-collapse" id="navbarColor03">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
-              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6">Master
-                List
+              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#services" style="border-color:#2EC4B6">Services
                 <span class="visually-hidden">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-
-              <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#"
-                style="border-color:#2EC4B6;">Payout</a>
+              <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#manage"
+                style="border-color:#2EC4B6;">Manage System</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#" style="border-color:#2EC4B6;">Contact
+              <a class="nav-link btn btn-outline-warning text-white ms-2 px-4" href="#about"
+                style="border-color:#2EC4B6;">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-outline-warning text-white ms-2" href="#contact" style="border-color:#2EC4B6;">Contact
                 Support</a>
             </li>
           </ul>
@@ -60,13 +64,81 @@
       <div class="text2">Supporting seniors with care and practical help to improve their well-being and independence.</div>
       <div class="logo"><img src="/DB31Project/SeniorSystemDBS/image/logo2.png" alt=""></div>
       <button class="login_btn" role="button" type="button">Login</button>
+      <div class="bottom"></div>
     </div>
-
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  <section class="services" id="services">
+    <img src="/DB31Project/SeniorSystemDBS/image/second_background.jpg" alt="">
+        <div class="heading5">
+            <h2 class>Services</h2>
+        </div>
+        <div class="servicesContent">
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src="/DB31Project/SeniorSystemDBS/image/list.png">
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2>Online Viewing</h2>
+                        <p>Our user-friendly online viewing for official list of the senior citizens allows senior citizens 
+                        to conveniently see their names from the comfort of their homes. Through a simple and intuitive process, seniors 
+                        can easily create an account through admin.</p>
+                    </div>                
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src="/DB31Project/SeniorSystemDBS/image/help.png">
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2>Personalized Assistance</h2>
+                        <p>Our dedicated support team is available to assist senior citizens throughout the registration process.
+                        We provide personalized guidance, answer queries, and offer technical assistance to ensure that a smooth 
+                        and hassle-free experience forindividuals.</p>
+                    </div>                
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src="/DB31Project/SeniorSystemDBS/image/payout.png">
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2>Payout Transaction</h2>
+                        <p>Once approved, senior citizens can receive their financial assistance through secure manual payout method.
+                        We prioritize the security of transactions and ensuring prompt and reliable disbursement of funds.</p>
+                    </div>                
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="manage" id="manage">
+      <div class="image_shape">
+
+      </div>
+      <img class="shapeimg" src="/DB31Project/SeniorSystemDBS/image/shapeimg.png" alt="">
+      <div class="manageContent">
+
+      </div>
+    </section>
+
+     <!-- footer section start -->
+     <!-- <footer>
+        <span>Created By <a href="https://www.facebook.com/cinlabsss">Cindy Labra</a>, <a href="https://www.facebook.com/profile.php?id=100008159077248">Ariel Abelgas & <a href="https://www.facebook.com/allyza.indiola">Allyza Indiola</a></a> | <span class="far fa-copyright"></span> 2023 All rights reserved.</span>
+    </footer> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+     <script src="/DB31Project/SeniorSystemDBS/js/swiper.js"></script>
 </body>
 
 </html>
